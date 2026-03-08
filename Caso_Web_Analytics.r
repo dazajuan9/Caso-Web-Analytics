edd8f6580d4bd010dbf56d5af03adb1b79ee9abf
MarianaaHernandez <he_mariana@javeriana.edu.co>
2026-03-08 18:28:15 -0500

    Se resuelve el punto 1
    Se crearon los graficos de: visitas semanasles, Ingresos semanales, Beneficios semanales y Lbs gastadas en publicidad semanalmente.

--- Caso_Web_Analytics.r
+++ Caso_Web_Analytics.r
@@ -18,3 +18,26 @@ head(combined_data)
 
 # Visualizar la estructura de los datos
 str(combined_data)
+
+#PUNTO 1: DATA ANALISIS
+# Grafico de visitas semanales 
+ggplot(combined_data, aes(x = `Week (2008-2009)`, y = `Unique Visits`, group = 1)) +
+  geom_col(fill = "steelblue", linewidth = 1) + labs (title = "Visitas Semanales a lo largo del tiempo", x = "Semana", y = "Número de Visitas") +
+  theme_minimal() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+###################################################################
+# Gráfico de Ingresos semanales 
+ggplot(combined_data, aes(x = `Week (2008-2009)`, y = `Revenue`, group = 1)) +
+  geom_col(fill = "purple", linewidth = 1) + labs (title = "Ingresos Semanales a lo largo del tiempo", x = "Semana", y = "Ingresos") +
+  theme_minimal() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+####################################################################
+# Gráfico de Beneficios semanales 
+ggplot(combined_data, aes(x = `Week (2008-2009)`, y = `Profit`, group = 1)) +
+  geom_col(fill = "green", linewidth = 1) + labs (title = "Beneficios Semanales a lo largo del tiempo", x = "Semana", y = "Beneficios") +
+  theme_minimal() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+####################################################################
+# Gráfico de Libras gastadas en publicidad semanalmente 
+ggplot(combined_data, aes(x = `Week (2008-2009)`, y = `Lbs. Sold`, group = 1)) +
+  geom_col(fill = "orange", linewidth = 1) +
+  labs(title = "Libras vendidas a lo largo del tiempo", x = "Semana", y = "Libras vendidas") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
